bb3fe8e6a7056a21f60af4bbada866426389f4b5
Monica Kristiadi <monica.kristiadi@tech.jago.com>
2022-10-10 22:12:12 +0700

    use immer to update state

--- src/store/wallet.js
+++ src/store/wallet.js
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import create from 'zustand';
 
 const initialUser = { first_name: 'Monica' };
@@ -6,15 +7,16 @@ const initialBalance = 100000;
 const useWalletStore = create((set) => ({
   user: initialUser,
   balance: initialBalance,
-  onDeposit: (amount) => set((state) => {
-    return { ...state, balance: state.balance + amount }
-  }),
-  onWithdraw: (amount) => set((state) => {
-    if (state.balance >= amount) {
-      return { ...state, balance: state.balance - amount };
-    }
-    return state;
-  }),
+  onDeposit: (amount) =>
+    set(produce((state) => {
+      state.balance = state.balance + amount
+    })),
+  onWithdraw: (amount) =>
+    set(produce((state) => {
+      if (state.balance >= amount) {
+        state.balance = state.balance - amount;
+      }
+    })),
 }));
 
 // selector bisa dibuat di sini, biar bisa reusesable
