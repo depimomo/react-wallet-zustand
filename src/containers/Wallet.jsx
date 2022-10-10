35c99799037004986e22609f8a5ac42bc808c4e7
Monica Kristiadi <monica.kristiadi@tech.jago.com>
2022-10-10 22:02:57 +0700

    able to deposit and withdraw

--- src/containers/Wallet.jsx
+++ src/containers/Wallet.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
-import useWalletStore, { selectBalance, selectUser } from '../store/wallet';
+import useWalletStore, { selectBalance, selectOnDeposit, selectOnWithdraw, selectUser } from '../store/wallet';
 
 const Wallet = () => {
   const user = useWalletStore(selectUser);
   const balance = useWalletStore(selectBalance);
+  const onDeposit = useWalletStore(selectOnDeposit);
+  const onWithdraw = useWalletStore(selectOnWithdraw);
 
   return (
     <div>
       <h1>{user.first_name} Wallet</h1>
       <h2>Balance: Rp {balance.toLocaleString('ID')}</h2>
-      <button>Withdraw Rp 10.000</button>
-      <button>Deposit Rp 10.000</button>
+      <button onClick={onWithdraw}>Withdraw Rp 10.000</button>
+      <button onClick={onDeposit}>Deposit Rp 10.000</button>
       <br />
       <br />
       <input/>
--- src/store/wallet.js
+++ src/store/wallet.js
@@ -5,11 +5,22 @@ const initialBalance = 100000;
 
 const useWalletStore = create((set) => ({
   user: initialUser,
-  balance: initialBalance
+  balance: initialBalance,
+  onDeposit: () => set((state) => {
+    return { ...state, balance: state.balance + 10000 }
+  }),
+  onWithdraw: () => set((state) => {
+    if (state.balance >= 10000) {
+      return { ...state, balance: state.balance - 10000 };
+    }
+    return state;
+  }),
 }));
 
 // selector bisa dibuat di sini, biar bisa reusesable
 export const selectBalance = (state) => state.balance;
 export const selectUser = (state) => state.user;
+export const selectOnDeposit = (state) => state.onDeposit;
+export const selectOnWithdraw = (state) => state.onWithdraw;
 
 export default useWalletStore;
